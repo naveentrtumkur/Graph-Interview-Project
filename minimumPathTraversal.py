0f4c67b0c103eb18a5b077677661a572252858bd
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-01-22 23:59:20 -0500

    modify

--- minimumPathTraversal.py
+++ minimumPathTraversal.py
@@ -21,6 +21,9 @@ def parse_infile(fPath):
 # tree to find min_path and printing the minimum path value calculated.
 constructTriangle = parse_infile("small_triangle.txt")
 
+# I'm using Dynamic programming to approach this problem.
+# I'm using a top-down approach to solve this problem.
+# Using a res, I will be avoiding overlapping subproblems.
 # Create a res list initialized to 0.
 res = [[0 for i in range(len(row))] for row in constructTriangle]
 res[0][0] = constructTriangle[0][0]
@@ -35,9 +38,14 @@ for i in range(1, len(constructTriangle)):
 print(min(res[-1]))
 #print(res)
 
+# The path traversal value would be stored in val and printed out at last.
 val = []
+
+# Initialise variables i and j to zero.
 i=0
 j=0
+
+# Update the list-val with the path traversal values.
 for i in range(len(res)):
     while j < (len(res[i])):
         if len(res[i])==1:
@@ -56,3 +64,4 @@ for i in range(len(res)):
 for i in val:
     print(i)
 
+###################################################### END OF THE PROGRAM #################################
